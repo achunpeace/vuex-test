e7224d39ed6d6d39e9cfba083d753efbc9d81c82
achun <achunpeace@163.com>
2020-04-02 18:49:34 +0800

    添加样式

--- src/components/Todos.vue
+++ src/components/Todos.vue
@@ -7,7 +7,7 @@
     </div>
 </template>
 <script>
-import {mapGetters} from 'vuex'
+import {mapGetters, mapActions} from 'vuex'
 export default {
     name: "Todos",
     computed: mapGetters({
@@ -18,11 +18,26 @@ export default {
     //         return this.$store.getters.getAllTodos
     //     }
     // }
+    methods: mapActions(['getAllTodos']),
     created(){
-        this.$store.dispatch("getAllTodos");
+        // this.$store.dispatch("getAllTodos");
+        this.getAllTodos();
     }
 }
 </script>
 <style scoped>
-
+    .todos {
+        display: grid;
+        grid-template-columns: repeat(3,1fr);
+        grid-gap:1rem;
+    }
+    .todo {
+        background: #41b883;
+        padding: 10px;
+        border: 1px solid #41b883;
+        border-radius: 5px;
+        position: relative;
+        cursor: pointer;
+        color: white;
+    }
 </style>
